25315d222bd0e4561bf6766e9291f7384a555996
kaunghtet <kaunghtet@WTPL-KAUNGHTET>
2020-01-16 15:53:39 +0800

    Add project files.

---
+++ Abc.Northwind.ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Abc.Northwind.Entities.Concrete;
+using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Business.Concrete;
+using Abc.Northwind.DataAccess.Concrete.EntityFramework;
+using Abc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Northwind.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+
+            foreach(var product in productService.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Abc.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Abc.Northwind.DataAccess.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using Abc.Core.DataAccess.EntityFramework;
+using Abc.Northwind.DataAccess.Concrete.EntityFramework.Contexts;
+
+namespace Abc.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+       
+    }
+}
